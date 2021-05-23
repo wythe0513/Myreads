7171d9be601d4b82c6af00eb2851f9c4b88c1fc1
wythe0513 <wythenshawe0513@gmail.com>
2021-05-23 09:41:56 +0000

    debounce added in Serce.js

--- src/Search.js
+++ src/Search.js
@@ -2,7 +2,9 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import * as BooksAPI from './BooksAPI';
-import ShelfChanger from './ShelfChanger.js'
+import ShelfChanger from './ShelfChanger'
+import { debounce } from 'lodash';
+
 
 class Search extends Component {
   static propTypes = {
@@ -16,11 +18,7 @@ class Search extends Component {
     searchErr: false
   };
 
-  getBooks = event => {
-    const query = event.target.value;
-    this.setState({ query });
-
-    
+   search=debounce((query)=>{  
     if (query) {
       BooksAPI.search(query.trim(), 20).then(books => {
         books.length > 0
@@ -30,8 +28,16 @@ class Search extends Component {
 
       
     } else this.setState({ newBooks: [], searchErr: false });
-  };
+  },400)
+ 
 
+  
+  getBooks = event => {
+    const query = event.target.value;
+    this.setState({ query });
+    this.search(query)
+  }
+ 
   render() {
     const { query, newBooks, searchErr } = this.state;
     const { books, changeShelf } = this.props;
@@ -51,41 +57,37 @@ class Search extends Component {
             />
           </div>
         </div>
+  
         <div className="search-books-results">
           {newBooks.length > 0 && (
             <div>
               <h3>Search returned {newBooks.length} books </h3>
               <ol className="books-grid">
                 {newBooks.map(newbook=>
-              <li key={newbook.id} className="book">
-
-         
+                <li key={newbook.id} className="book">       
            
-           <div className="book-top">
-             <div className="book-cover" 
-	          style={{ 
-                width: 128, 
-                height: 193, 
-                backgroundImage: `url(${newbook.imageLinks.thumbnail})` 
-              }}>
-              </div>
-              <ShelfChanger book={newbook} books={books} changeShelf={changeShelf}/>
-             
-           </div>
+                 <div className="book-top">
+                  <div className="book-cover" 
+	                style={{ 
+                    width: 128, 
+                    height: 193, 
+                    backgroundImage: `url(${newbook.imageLinks.thumbnail})` 
+                    }}>
+                  </div>
+                  <ShelfChanger book={newbook} books={books} changeShelf={changeShelf}/>             
+                  </div>
 
-          <div className="book-title">{newbook.title}</div>
-          <div className="book-authors">{newbook.authors}</div>
-        
-        
-         
-       </li>
-      )}
-              </ol>
-            </div>
-          )}
-          {searchErr && (
+                  <div className="book-title">{newbook.title}</div>
+                  <div className="book-authors">{newbook.authors}</div>       
+                </li>
+                )}
+             </ol>
+             </div>
+             )}
+           
+           {searchErr && (
             <h3>Search did not return any books. Please try again!</h3>
-          )}
+           )}
         </div>
       </div>
     );
